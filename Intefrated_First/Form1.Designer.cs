d164d0bc71f1fa55ec986f01cdcbd9e9ddab4797
D-Moskalyov <moscalevd@Gmail.com>
2015-10-11 21:12:03 +0300

    initial

---
+++ Intefrated_First/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Intefrated_First
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(261, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Show all employees";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(261, 368);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(311, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(360, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(261, 368);
+            this.listBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(360, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(261, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Show all customers";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 414);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(609, 175);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(279, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Show Deals";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 601);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Intefrated_First/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Intefrated_First
+{
+    public partial class Form1 : Form
+    {
+        string connectionSrting = "server=localhost;integrated security=true;database=northwind";
+        SqlConnection connection;
+        SqlCommand commamd;
+        SqlDataReader reader;
+        DataTable sqlTable;
+        SqlDataAdapter adapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (connection == null || connection.State != ConnectionState.Open)
+            {
+                connection = new SqlConnection(connectionSrting);
+                try
+                {
+                    connection.Open();
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        label1.BackColor = Color.Green;
+                        //string cmd = "select * from Employees";
+                        //SqlCommand commamd = new SqlCommand(cmd, connection);
+                        //SqlDataReader reader = commamd.ExecuteReader();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Connection failed");
+                    }
+                }
+                catch (SqlException ee)
+                {
+                    MessageBox.Show("Connection failed");
+                    MessageBox.Show(String.Format("Server {0}", ee.Server));
+                    MessageBox.Show(ee.Message);
+                }
+            }
+            //finally
+            //{
+            //    connection.Close();
+            //}
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+                connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                label1.BackColor = Color.Red;
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                listBox1.Items.Clear();
+                string cmd = "select * from Employees";
+                commamd = new SqlCommand(cmd, connection);
+                reader = commamd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listBox1.Items.Add((string)reader[2] + " " + (string)reader[1]);
+                }
+                reader.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                string name = listBox1.SelectedItem.ToString();
+                int num = name.IndexOf(" ");
+                char[] lastName = name.ToCharArray(num + 1, name.Length - num - 1);
+                string last = new string(lastName);
+                string cmd = "select * from Employees where LastName = '" + last + "'";
+                //MessageBox.Show(cmd);
+                SqlCommand commamd = new SqlCommand(cmd, connection);
+                SqlDataReader reader = commamd.ExecuteReader();
+                reader.Read();
+                Form2 form2 = new Form2();
+                form2.Show();
+                Label lb;
+                TextBox tB;
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    lb = new Label();
+                    lb.Location = new System.Drawing.Point(5, i * 20 + i * 2 + 5);
+                    lb.AutoSize = true;
+                    lb.Text = reader.GetName(i) + ":";
+
+                    tB = new TextBox();
+                    tB.Location = new System.Drawing.Point(90, i * 20 + i * 2 + 5);
+                    tB.Size = new System.Drawing.Size(222, 20);
+                    tB.ReadOnly = true;
+                    tB.Text = reader[i].ToString();
+
+                    form2.Controls.Add(lb);
+                    form2.Controls.Add(tB);
+                    //Console.WriteLine("{0}: {1}", reader.GetName(i), reader[i]);
+                }
+                reader.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                listBox2.Items.Clear();
+                string cmd = "select * from Customers";
+                commamd = new SqlCommand(cmd, connection);
+                reader = commamd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listBox2.Items.Add((string)reader[1]);
+                }
+                reader.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                ListBox.SelectedObjectCollection emp = listBox1.SelectedItems;
+                ListBox.SelectedObjectCollection cust = listBox2.SelectedItems;
+                if (emp.Count != 0 && cust.Count != 0)
+                {
+                    string cmd = "select * from Customers";
+                    //            select OrderID, o.CustomerID, o.EmployeeID, OrderDate
+                    //from Orders as o
+                    //join
+                    //Employees as e
+                    //on 
+                    //o.EmployeeID = e.EmployeeID
+                    //join
+                    //Customers as c
+                    //on o.CustomerID = c.CustomerID
+                    //where (c.ContactName = '')
+                    //and (e.LastName = '')
+                    commamd = new SqlCommand(cmd, connection);
+                    sqlTable = new DataTable();
+                    adapter = new SqlDataAdapter();
+                    adapter.SelectCommand = commamd;
+                    adapter.Fill(sqlTable);
+                    dataGridView1.DataSource = sqlTable;
+                }
+            }
+        }
+    }
+}
